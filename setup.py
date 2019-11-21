3bb4564cd43cb44f32396fc5140d51a90f8b5b95
gantian127 <jamy127@foxmail.com>
2019-11-21 11:38:19 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pkg_temp", # Replace with your own username
+    name="pkg_temp",  # Replace with your own username
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/",
+    url="https://github.com/", # github repo for your package
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
