c30069577bc9628330ed9f3ba85a1bf99778a58a
gantian127 <jamy127@foxmail.com>
2020-03-06 16:46:34 -0700

    add travis ci

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pkg_temp",  # Replace with your own username
-    version="0.0.1",
+    version="0.1.0",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
